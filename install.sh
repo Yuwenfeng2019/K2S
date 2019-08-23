4521e08c4fa545166485d4c93ab0a07a1c7afd7a
Joakim Roubert <joakimr@axis.com>
2019-08-23 10:29:36 +0200

    Manually merge 'install.sh: Use -eq instead of = for integer comparison' from K3S

--- install.sh
+++ install.sh
@@ -167,7 +167,7 @@ setup_env() {
 
     # --- use sudo if we are not already root ---
     SUDO=sudo
-    if [ $(id -u) = 0 ]; then
+    if [ `id -u` -eq 0 ]; then
         SUDO=
     fi
 
@@ -383,7 +383,7 @@ create_killall() {
     $SUDO tee ${BIN_DIR}/${KILLALL_K2S_SH} >/dev/null << \EOF
 #!/bin/sh
 set -x
-[ $(id -u) = 0 ] || exec sudo $0 $@
+[ `id -u` -eq 0 ] || exec sudo $0 $@
 
 for bin in /var/lib/yuwenfeng2019/k2s/data/**/bin/; do
     [ -d $bin ] && export PATH=$bin:$PATH
@@ -439,7 +439,7 @@ create_uninstall() {
     $SUDO tee ${BIN_DIR}/${UNINSTALL_K2S_SH} >/dev/null << EOF
 #!/bin/sh
 set -x
-[ \$(id -u) = 0 ] || exec sudo \$0 \$@
+[ \`id -u\` -eq 0 ] || exec sudo \$0 \$@
 
 ${BIN_DIR}/${KILLALL_K2S_SH}
 
