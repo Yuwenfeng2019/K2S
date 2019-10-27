8021535bd4b5e287275f6c02fe576a7b00d1800f
Darren Shepherd <darren@rancher.com>
2019-10-26 22:53:25 -0700

    Manually merge 'Refactor tokens, bootstrap, and cli args' from K3S

--- pkg/server/types.go
+++ pkg/server/types.go
@@ -1,14 +1,12 @@
 package server
 
 import (
-	"github.com/rancher/dynamiclistener"
 	"github.com/Yuwenfeng2019/K2S/pkg/daemons/config"
 )
 
 type Config struct {
 	DisableAgent     bool
 	DisableServiceLB bool
-	TLSConfig        dynamiclistener.UserConfig
 	ControlConfig    config.Control
 	Rootless         bool
 }
